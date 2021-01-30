a4e48697d4a05086cdcb16624ff9dc8d8cd9223b
Juliana <julianadecarvalho92@gmail.com>
2021-01-30 13:49:00 -0500

    Initial commit

---
+++ Slot Machine/Slot Machine/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by Juliana de Carvalho on 2021-01-30.
+//  Copyright © 2021 Juliana de Carvalho. All rights reserved.
+//  Student 1: Abdeali Mody - Student Id: 301085484
+//  Student 2: Juliana de Carvalho - Student Id: 301137060
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
